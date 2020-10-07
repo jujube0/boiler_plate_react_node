5b3100c37216323a6c52e2ffcfaf603d16e74fc9
GaYoung <kians2@naver.com>
2020-10-07 18:29:09 +0900

    로그인 완성

--- models/User.js
+++ models/User.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
+
+const saltRounds = 10
+const jwt = require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({
     name : {
@@ -26,6 +30,54 @@ const userSchema = mongoose.Schema({
         type : Number
     }
 })
+
+// save 전에 func 을 하도록
+userSchema.pre('save', function( next ){
+
+    var user = this;
+
+
+    // 비밀번호를 바꿀 때만 암호화를 시행하도록
+    if(user.isModified('password')){
+        //  비밀번호를 암호화 시킨다
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if(err) return next(err);
+
+            // 첫번째 argument 실제 비밀번호
+            // hash : 암호화된 비밀번호
+            bcrypt.hash(user.password , salt , function(err, hash){
+                if(err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    }else{
+        next()
+    }
+    
+})
+
+// cb : call back 
+userSchema.methods.comparePassword = function(plainPassword, cb){
+    // plainPassword를 암호화된 비밀번호와 일치하는지
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return cb(err);
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb){
+    var user = this;
+    // json web token 이용해서 token 생성하기
+    // user id 와 두번째 param 으로 토큰을 만들고, param 을 이용하여 나중에 userid를 찾아낸다. 
+    var token = jwt.sign(user._id.toHexString(), "secretToken")
+
+    user.token = token;
+    user.save(function(err, user){
+        if(err) return cb(err);
+        cb(null, user)
+    })
+}
 
 const User = mongoose.model('User',userSchema)
 
